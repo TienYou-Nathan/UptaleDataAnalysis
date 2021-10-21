96ca74bcca6d26883e24f24d28bce51c47f43e65
Nathan Tien You <nathan.tienyou@viacesi.fr>
2021-10-21 12:45:04 +0200

    Added tags clicks avg + scenevisit click avg requests

--- requetes-lea.sql
+++ requetes-lea.sql
@@ -156,3 +156,102 @@ WHERE Users.Id IN (
         GROUP BY UserId
         ORDER BY StartTime
     );
+
+
+-- IDK
+SELECT 
+	SceneVisit.SessionId,
+	Topics.Name,
+	Scenes.Name,
+	Categories.Name
+FROM SceneVisit
+LEFT JOIN Scenes on Scenes.Id = SceneVisit.SceneId
+LEFT JOIN Categories ON Scenes.CategoryId = Categories.Id
+LEFT JOIN TopicsClicks On TopicsClicks.SessionId = SceneVisit.SessionId
+LEFT JOIN Topics On Topics.Id = TopicsClicks.TopicId
+WHERE SceneVisit.SessionId = "00b51d68-406f-4f91-84ee-2f6a7393f634" AND Categories.Name != "Obligatoire" AND Topics.Name NOT LIKE "Masquer%"
+ORDER BY SceneVisit.EnterTime ASC;
+
+SELECT * FROM TopicsClicks
+LEFT JOIN Topics On Topics.Id = TopicsClicks.TopicId
+LIMIT 100;
+
+
+-- En globalité, quels sont les tags les plus cliqués par chaque groupe
+WITH 
+AllClicks AS (
+SELECT 
+	Topics.Name AS "TopicName",
+	Topics.Id AS "TopicId",
+	Scenes.Name AS "SceneName",
+	Scenes.Id AS "SceneId",
+  	Categories.Name AS "CategoryName",
+  	Categories.Color AS "CategoryColor",
+  	Categories.Id AS "CategoryId",
+	Users.Id AS "UserId",
+	UsersGroups.Name AS "UserGroupName",
+	UsersGroups.Id AS "UserGroupId",
+	ROW_NUMBER() OVER (
+                Partition by Users.Id
+            ) TopicOrder
+
+FROM TopicsClicks
+LEFT JOIN Topics ON TopicsClicks.TopicId = Topics.Id
+LEFT JOIN Scenes ON Topics.SceneId = Scenes.Id
+LEFT JOIN Categories ON Categories.Id = Scenes.CategoryId
+LEFT JOIN Sessions ON Sessions.Id = TopicsClicks.SessionId
+LEFT JOIN Users ON Sessions.UserId = Users.Id
+LEFT JOIN UserGroupAssociation ON Users.Id = UserGroupAssociation.UserId
+LEFT JOIN UsersGroups ON UserGroupAssociation.UserGroupId = UsersGroups.Id
+WHERE UsersGroups.Type = "Expertise"  AND Users.Id NOT LIKE "Léa Frigo%")
+
+
+SELECT 
+	TopicName, SceneName, CategoryName,
+	COUNT(CASE WHEN UserGroupName = "M1" THEN UserGroupName END) AS "M1 Clicks",
+	COUNT(CASE WHEN UserGroupName = "M2" THEN UserGroupName END) AS "M2 Clicks",
+	COUNT(CASE WHEN UserGroupName = "M1" THEN UserGroupName END) - COUNT(CASE WHEN UserGroupName = "M2" THEN UserGroupName END) AS "Clicks Difference",
+    AVG(CASE WHEN UserGroupName = "M1" THEN TopicOrder END) AS "M1 Order Avg",
+	AVG(CASE WHEN UserGroupName = "M2" THEN TopicOrder END) AS "M2 Order Avg",
+	AVG(CASE WHEN UserGroupName = "M1" THEN TopicOrder END) - AVG(CASE WHEN UserGroupName = "M2" THEN TopicOrder END) AS "Order Difference"
+	FROM AllClicks
+	WHERE CategoryName != "Obligatoire" AND CategoryName != "Tutoriel" AND TopicName NOT LIKE "Masquer%"
+GROUP BY TopicId
+ORDER BY "Order Difference";
+
+
+--En Globalité, quelles sont les scènes les plus visitées par chaque groupe
+WITH Visites AS
+
+(SELECT 
+ 	UsersGroups.Id AS "UsersGroupsId",
+	UsersGroups.Name AS "UserGroupName",
+ 	Scenes.Name AS "SceneName",
+ 	Scenes.Id AS "SceneId",
+ 	Categories.Name AS "CategoryName",
+ 	Categories.Id AS "Categories",
+	
+	ROW_NUMBER() OVER (
+                Partition by Users.Id
+            ) SceneOrder
+			
+FROM SceneVisit
+LEFT JOIN Sessions ON Sessions.Id = SceneVisit.SessionId
+LEFT JOIN Users ON Sessions.UserId = Users.Id
+LEFT JOIN UserGroupAssociation ON Users.Id = UserGroupAssociation.UserId
+LEFT JOIN UsersGroups ON UserGroupAssociation.UserGroupId = UsersGroups.Id
+LEFT JOIN Scenes ON Scenes.Id = SceneVisit.SceneId
+LEFT JOIN Categories ON Categories.Id = Scenes.CategoryId
+WHERE UsersGroups.Type = "Expertise" AND Users.Id NOT LIKE "Léa Frigo%" AND Categories.Name != "Obligatoire" AND Categories.Name != "Tutoriel")
+
+SELECT 
+	SceneName,
+	COUNT(CASE WHEN UserGroupName = "M1" THEN UserGroupName END) AS "M1 Visits",
+	COUNT(CASE WHEN UserGroupName = "M2" THEN UserGroupName END) AS "M2 Visits",
+	COUNT(CASE WHEN UserGroupName = "M1" THEN UserGroupName END) - COUNT(CASE WHEN UserGroupName = "M2" THEN UserGroupName END) AS "Visits Difference",
+	AVG(CASE WHEN UserGroupName = "M1" THEN SceneOrder END) AS "M1 Order Avg",
+	AVG(CASE WHEN UserGroupName = "M2" THEN SceneOrder END) AS "M2 Order Avg",
+	AVG(CASE WHEN UserGroupName = "M1" THEN SceneOrder END) - AVG(CASE WHEN UserGroupName = "M2" THEN SceneOrder END) AS "Order Difference"
+FROM Visites 
+GROUP BY SCeneId
+ORDER BY "Order Difference";
